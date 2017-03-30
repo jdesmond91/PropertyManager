c320aa6fd5c77bb6637ce15c580dac6de6d7d520
amandamc4 <amanda.mc4@hotmail.com>
2017-03-30 10:09:44 -0400

    added icon fix for edit pages, validation fix for service address

--- PropertyManager/App_Start/BundleConfig.cs
+++ PropertyManager/App_Start/BundleConfig.cs
@@ -67,6 +67,7 @@ namespace PropertyManager
                       "~/app/Services/unitService.js",
                       "~/app/Services/unitphotoService.js",
                       "~/app/Services/userProfile.js",
+                      "~/app/Services/userService.js",
                       "~/app/Services/workOrderService.js",
                       //CONTROLLERS
                       "~/app/Controllers/announcementController.js",
@@ -85,6 +86,7 @@ namespace PropertyManager
                       "~/app/Controllers/tenantController.js",
                       "~/app/Controllers/unitController.js",
                       "~/app/Controllers/unitphotoController.js",
+                      "~/app/Controllers/userController.js",
                       "~/app/Controllers/workOrderController.js",
                       "~/app/Controllers/workOrderManagerController.js"
             ));
