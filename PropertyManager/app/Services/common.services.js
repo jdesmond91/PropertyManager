a2918b7befca902b8ac13396077b38c801eb8ec2
amandamc4 <amanda.mc4@hotmail.com>
2017-04-10 22:28:25 -0400

    required laundry etc.

--- PropertyManager/app/Services/common.services.js
+++ PropertyManager/app/Services/common.services.js
@@ -1,6 +1,6 @@
 ﻿//Made by Jonathan Desmond
 
 angular.module("common.services", ["ngResource"]).constant("appSettings", {
-    //serverPath: "http://propertycloud1.azurewebsites.net"
-    serverPath: "http://localhost:24792"
+    serverPath: "http://propertycloud1.azurewebsites.net"
+    //serverPath: "http://localhost:24792"
 });
